1939d2d000d3fa3b3924261648848edef8946a3a
fgdghthr45r <fdghnrge5@atomicmail.io>
2026-07-25 21:23:03 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,12 @@ WEB_PORT=3000
 echo "🚀 Starting X-UI on port ${WEB_PORT}..."
 
 # تنظیم X-UI روی پورت ۳۰۰۰
-/usr/local/x-ui/x-ui setting -port ${WEB_PORT} -webBasePath /xui
+#/usr/local/x-ui/x-ui setting -port ${WEB_PORT} -webBasePath /xui
+
+./x-ui setting -port 2053 -webBasePath /managepanel/ || true
+
+
+
 echo "🔧 Building nginx.conf for fixed port: $NGINX_PORT"
 envsubst '${NGINX_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 # اجرای مستقیم X-UI
